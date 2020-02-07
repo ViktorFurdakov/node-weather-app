b3f581c45907d36cdf71b9f9839834ae817afdda
ViktorFurdakov <vfurdakov@aitheon.com>
2020-02-07 09:37:25 +0200

    Heroku set up

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const forecast = require('./utils/forecast');
 const hbs = require('hbs');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 const publicDir = path.join(__dirname, '../public');
 const templatesPath = path.join(__dirname, '../templates/views');
@@ -87,6 +88,6 @@ app.get('*', (req, res) => {
   })
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Started!');
 });
